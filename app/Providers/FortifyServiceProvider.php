9055f73d10dccea1e5dc2e942b82aed7748a70c7
Andi Zunaidi <zunaidius@gmail.com>
2023-09-23 13:37:15 +0700

    set template dan fortify

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -42,5 +42,25 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
+
+        Fortify::loginView(function () {
+            return view('auth.login');
+        });
+
+        Fortify::registerView(function () {
+            return view('auth.register');
+        });
+
+        Fortify::requestPasswordResetLinkView(function () {
+            return view('auth.forgot');
+        });
+
+        Fortify::verifyEmailView(function () {
+            return view('auth.verify');
+        });
+
+        Fortify::resetPasswordView(function (Request $request) {
+            return view('auth.reset', ['request' => $request]);
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,34 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('auth.login', ['type_menu' => '']);
 });
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.blank-page', ['type_menu' => '']);
+    })->name('home');
+});
+
+
+// Route::get('/login', function () {
+//     // return view('welcome');
+//     return view('auth.login');
+// });
+
+// Route::get('/register', function () {
+//     return view('auth.register');
+// });
+
+// Route::get('/reset', function () {
+//     return view('auth.reset');
+// });
+
+// Route::get('/forgot', function () {
+//     return view('auth.forgot');
+// });
+
+// Route::get('/verify', function () {
+//     return view('auth.verify');
+// });
